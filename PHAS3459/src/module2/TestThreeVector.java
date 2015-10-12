7516541f9c8f2a2ac55add846d5e37fabf2dc4a4
Liam <kingliam28@ntlworld.com>
2015-10-12 20:59:52 +0100

    Implemented static methods within the unstatic methods

--- PHAS3459/src/module2/TestThreeVector.java
+++ PHAS3459/src/module2/TestThreeVector.java
@@ -1,40 +1,40 @@
 package module2;
 
 public class TestThreeVector {
-	public static void main(String[] args){
-		ThreeVector v1 = new ThreeVector(4,3,3);
-		ThreeVector v2 = new ThreeVector(3,5,2);
-		ThreeVector v3 = new ThreeVector(0,0,0);
-		
+	public static void main(String[] args) {
+		ThreeVector v1 = new ThreeVector(4, 3, 3);
+		ThreeVector v2 = new ThreeVector(3, 5, 2);
+		ThreeVector v3 = new ThreeVector(0, 0, 0);
+
 		System.out.println(v1 + "\n" + v1.unitVector() + "\n");
 		System.out.println(v2 + "\n" + v2.unitVector() + "\n");
 		System.out.println(v3 + "\n" + v3.unitVector());
-		
-		//Static
+
+		// Scalar Product
 		System.out.println(ThreeVector.scalarProduct(v1, v2));
 		System.out.println(ThreeVector.scalarProduct(v1, v3));
-		
-		System.out.println(ThreeVector.vectorProduct(v1, v2));
-		System.out.println(ThreeVector.vectorProduct(v1, v3));
-		
-		System.out.println(ThreeVector.angle(v1, v2));
-		System.out.println(ThreeVector.angle(v1, v3));
-		
-		//Non-Static
+
 		System.out.println(v1.scalarProduct(v2));
 		System.out.println(v1.scalarProduct(v3));
-		
+
+		// Vector Product
+		System.out.println(ThreeVector.vectorProduct(v1, v2));
+		System.out.println(ThreeVector.vectorProduct(v1, v3));
+
 		System.out.println(v1.vectorProduct(v2));
 		System.out.println(v1.vectorProduct(v3));
 
+		// Angle
+		System.out.println(ThreeVector.angle(v1, v2));
+		System.out.println(ThreeVector.angle(v1, v3));
+
 		System.out.println(v1.angle(v2));
 		System.out.println(v1.angle(v3));
-		
-		//Printing out without toString() method.
+
+		// Printing out without toString() method.
 		System.out.println(v1);
 		System.out.println("Something to do with the memory, ***LOOK THIS UP LATER***");
-		
+
 	}
-	
-	
+
 }
--- PHAS3459/src/module2/ThreeVector.java
+++ PHAS3459/src/module2/ThreeVector.java
@@ -9,7 +9,7 @@ public class ThreeVector {
 		this.x = x;
 		this.y = y;
 		this.z = z;
-		
+
 	}
 
 	public double getX() {
@@ -23,55 +23,59 @@ public class ThreeVector {
 	public double getZ() {
 		return z;
 	}
-	
-	double magnitude(){
+
+	double magnitude() {
 		return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
 	}
-	
-	ThreeVector unitVector(){
-		double normx = this.x/this.magnitude();
-		double normy = this.y/this.magnitude();
-		double normz = this.z/this.magnitude();
-		
+
+	ThreeVector unitVector() {
+		double normx = this.x / this.magnitude();
+		double normy = this.y / this.magnitude();
+		double normz = this.z / this.magnitude();
+
 		return new ThreeVector(normx, normy, normz);
-		
+
 	}
-	
-	public String toString(){
-		return "x: " + this.x +" y: " + this.y + " z: " + this.z;
+
+	public String toString() {
+		return "x: " + this.x + " y: " + this.y + " z: " + this.z;
 	}
-	
-	static double scalarProduct(ThreeVector threeVec1, ThreeVector threeVec2){
-		return threeVec1.x*threeVec2.x + threeVec1.y*threeVec2.y + threeVec1.z*threeVec2.z;
+
+	static double scalarProduct(ThreeVector threeVec1, ThreeVector threeVec2) {
+		return threeVec1.x * threeVec2.x + threeVec1.y * threeVec2.y + threeVec1.z * threeVec2.z;
 	}
-	
-	static ThreeVector vectorProduct(ThreeVector threeVec1, ThreeVector threeVec2){
-		return new ThreeVector(threeVec1.y*threeVec2.z - threeVec1.z*threeVec2.y, threeVec1.z*threeVec2.x - threeVec1.x*threeVec2.z, threeVec1.x*threeVec2.y - threeVec1.y*threeVec2.x);
+
+	static ThreeVector vectorProduct(ThreeVector threeVec1, ThreeVector threeVec2) {
+		return new ThreeVector(threeVec1.y * threeVec2.z - threeVec1.z * threeVec2.y,
+				threeVec1.z * threeVec2.x - threeVec1.x * threeVec2.z,
+				threeVec1.x * threeVec2.y - threeVec1.y * threeVec2.x);
 	}
-	
-	static ThreeVector add(ThreeVector threeVec1, ThreeVector threeVec2){
-		return new ThreeVector(threeVec1.x+threeVec2.x, threeVec1.y + threeVec2.y, threeVec1.z + threeVec2.z);
+
+	static ThreeVector add(ThreeVector threeVec1, ThreeVector threeVec2) {
+		return new ThreeVector(threeVec1.x + threeVec2.x, threeVec1.y + threeVec2.y, threeVec1.z + threeVec2.z);
 	}
-	
-	static double angle(ThreeVector threeVec1, ThreeVector threeVec2){
-		return Math.acos(ThreeVector.scalarProduct(threeVec1, threeVec2) / (threeVec1.magnitude()*threeVec2.magnitude()));
+
+	static double angle(ThreeVector threeVec1, ThreeVector threeVec2) {
+		return Math.acos(
+				ThreeVector.scalarProduct(threeVec1, threeVec2) / (threeVec1.magnitude() * threeVec2.magnitude()));
 	}
-	
-	double scalarProduct(ThreeVector threeVec){
-		return threeVec.x*this.x + threeVec.y*this.y + threeVec.z*this.z;
+
+	double scalarProduct(ThreeVector threeVec) {
+		return ThreeVector.scalarProduct(this, threeVec);
 	}
-	
-	ThreeVector vectorProduct(ThreeVector threeVec){
-		return new ThreeVector(threeVec.y*this.z - threeVec.z*this.y, threeVec.z*this.x - threeVec.x*this.z, threeVec.x*this.y - threeVec.y*this.x);
-		
+
+	ThreeVector vectorProduct(ThreeVector threeVec) {
+		return ThreeVector.vectorProduct(this, threeVec);
+
 	}
-	ThreeVector add(ThreeVector threeVec){
-		return new ThreeVector(threeVec.x + this.x, threeVec.y + this.y, threeVec.z + this.z);
+
+	ThreeVector add(ThreeVector threeVec) {
+		return ThreeVector.add(this, threeVec);
 	}
-	
-	double angle(ThreeVector threeVec){
-		return  Math.acos(ThreeVector.scalarProduct(threeVec, this) / (threeVec.magnitude()*this.magnitude()));
-		
+
+	double angle(ThreeVector threeVec) {
+		return ThreeVector.angle(this, threeVec);
+
 	}
 
 }
