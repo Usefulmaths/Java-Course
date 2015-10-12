37ec025b11705d3d3d486b677e26e1aa681ce52c
Liam <kingliam28@ntlworld.com>
2015-10-12 15:50:32 +0100

    Added two new classes, Complex and TestComplex

--- PHAS3459/src/module2/ThreeVector.java
+++ PHAS3459/src/module2/ThreeVector.java
@@ -25,7 +25,7 @@ public class ThreeVector {
 	}
 	
 	double magnitude(){
-		return Math.sqrt(this.x * this.x + this.y*this.y + this.z * this.z);
+		return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
 	}
 	
 	ThreeVector unitVector(){
