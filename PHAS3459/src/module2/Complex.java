be124714f3e7398a7774bce3ad6eb5b8f977b801
Liam <kingliam28@ntlworld.com>
2015-10-13 09:41:34 +0100

    Method equals from comparing Complex objects to comparing components

--- PHAS3459/src/module2/Complex.java
+++ PHAS3459/src/module2/Complex.java
@@ -3,7 +3,7 @@ package module2;
 public class Complex {
 	double real;
 	double imag;
-	
+
 	static Complex ONE = new Complex(1, 0);
 	static Complex ZERO = new Complex(0, 0);
 	static Complex I = new Complex(0, 1);
@@ -37,38 +37,38 @@ public class Complex {
 		return new Complex(this.real / this.modulus(), this.imag / this.modulus());
 	}
 
-	/**CHECK THIS**/
 	boolean equals(Complex c) {
-		if (this == c) {
+		if (this.real == c.real && this.imag == c.imag) {
 			return true;
 		} else {
 			return false;
 		}
 	}
-	
-	public String toString(){
-		return this.real +" + " + this.imag + "i";
+
+	public String toString() {
+		return this.real + " + " + this.imag + "i";
 	}
-	
-	void setFromModulusAngle(double mag, double ang){
+
+	void setFromModulusAngle(double mag, double ang) {
 		this.real = mag * Math.cos(ang);
 		this.imag = mag * Math.sin(ang);
 	}
-	
-	static Complex add(Complex c1, Complex c2){
-		return new Complex(c1.real+c2.real, c1.imag+c2.imag);
+
+	static Complex add(Complex c1, Complex c2) {
+		return new Complex(c1.real + c2.real, c1.imag + c2.imag);
 	}
-	
-	static Complex subtract(Complex c1, Complex c2){
+
+	static Complex subtract(Complex c1, Complex c2) {
 		return new Complex(c1.real - c2.real, c1.imag - c2.imag);
 	}
-	
-	static Complex multiply(Complex c1, Complex c2){
-		return new Complex(c1.real*c2.real - c1.imag*c2.imag, c1.real*c2.imag + c2.real*c1.imag);
+
+	static Complex multiply(Complex c1, Complex c2) {
+		return new Complex(c1.real * c2.real - c1.imag * c2.imag, c1.real * c2.imag + c2.real * c1.imag);
 	}
-	static Complex divide(Complex c1, Complex c2){
-		return new Complex(Complex.multiply(c1, c2.conjugate()).real/ (c2.real*c2.real + c2.imag*c2.imag),Complex.multiply(c1, c2.conjugate()).imag/ (c2.real*c2.real + c2.imag*c2.imag));
+
+	static Complex divide(Complex c1, Complex c2) {
+		return new Complex(Complex.multiply(c1, c2.conjugate()).real / (c2.real * c2.real + c2.imag * c2.imag),
+				Complex.multiply(c1, c2.conjugate()).imag / (c2.real * c2.real + c2.imag * c2.imag));
 	}
-	
 
 }
--- PHAS3459/src/module2/ParticleMain.java
+++ PHAS3459/src/module2/ParticleMain.java
@@ -3,6 +3,7 @@ package module2;
 public class ParticleMain {
 	public static void main(String[] args){
 		FallingParticle particle = new FallingParticle(4.8,3.2);
+		
 		particle.setZ(6);
 		particle.drop(0.5);
 		
