5cf5eeca308993a8c8d512c97b2dbd85752e92c4
Liam <kingliam28@ntlworld.com>
2015-10-17 15:52:47 +0100

    Corrected modifiers and commented on what methods and code does.

--- PHAS3459/src/module2/FallingParticle.java
+++ PHAS3459/src/module2/FallingParticle.java
@@ -1,55 +1,66 @@
 package module2;
 
 public class FallingParticle {
-	double m;
-	double d;
-	double t = 0;
-	double z = 10;
-	double v = 0;
-	double g = 9.81;
-	
-	public FallingParticle(double m, double d){
+	// Field variables
+	private final double m;
+	private final double d;
+	private double t = 0;
+	public double z;
+	private double v = 0;
+	private final double g = 9.81;
+
+	// Constructor to set/store mass (kg) and drag coefficient (no units) of
+	// particle
+	public FallingParticle(final double m, final double d) {
 		this.m = m;
 		this.d = d;
 	}
 
+	// Returns time (seconds)
 	public double getT() {
 		return t;
 	}
 
+	// Sets time (seconds)
 	public void setT(double t) {
 		this.t = t;
 	}
 
+	// Returns height (metres)
 	public double getZ() {
 		return z;
 	}
 
+	// Sets height (metres)
 	public void setZ(double z) {
 		this.z = z;
 	}
 
+	// Returns velocity (metres per second)
 	public double getV() {
 		return v;
 	}
 
+	// Sets velocity (metres per second)
 	public void setV(double v) {
 		this.v = v;
 	}
 
-	void doTimeStep(double deltaT){
-		double a = d*v*v/m - g;
-		
-		this.setV(this.getV() + a*deltaT);
-		this.setZ(this.getZ() + v*deltaT);
+	// Calculates new V, Z, and T due to change in acceleration
+	// in deltaT time steps
+	public void doTimeStep(final double deltaT) {
+		double a = d * v * v / m - g;
+
+		this.setV(this.getV() + a * deltaT);
+		this.setZ(this.getZ() + v * deltaT);
 		this.setT(this.getT() + deltaT);
 	}
-	
-	void drop(double deltaT){
-		while(this.z > 0){
+
+	// Runs doTimeStep() until particles reaches a height of 0 (ground).
+	public void drop(final double deltaT) {
+		while (this.z > 0) {
 			this.doTimeStep(deltaT);
 		}
-		System.out.println(this.getT());
 	}
-	
+
 }
--- PHAS3459/src/module2/ParticleMain.java
+++ PHAS3459/src/module2/ParticleMain.java
@@ -1,34 +1,44 @@
 package module2;
 
 public class ParticleMain {
-	public static void main(String[] args){
-		FallingParticle particle = new FallingParticle(4.8,3.2);
-		
+	public static void main(String[] args) {
+		FallingParticle particle = new FallingParticle(4.8, 3.2);
+
+		// Particle dropping from height 6m in time steps of 0.5s
 		particle.setZ(6);
 		particle.drop(0.5);
-		
-		
+		System.out.println("Time taken: " + particle.getT() + "\t Velocity: " + particle.getV());
+
+		// Particle dropping from height 6m in time steps of 0.1s
 		particle.setZ(6);
 		particle.setT(0);
 		particle.setV(0);
 		particle.drop(0.1);
-		
+		System.out.println("Time taken: " + particle.getT() + "\t Velocity: " + particle.getV());
+
+		// Particle dropping from height 6m in time steps of 0.01s
 		particle.setZ(6);
 		particle.setT(0);
 		particle.setV(0);
 		particle.drop(0.01);
-		
+		System.out.println("Time taken: " + particle.getT() + "\t Velocity: " + particle.getV());
+
+		// Particle dropping from height 6m in time steps of 0.001s
 		particle.setZ(6);
 		particle.setT(0);
 		particle.setV(0);
 		particle.drop(0.001);
-		
+		System.out.println("Time taken: " + particle.getT() + "\t Velocity: " + particle.getV());
+
+		// Particle dropping from height 6m in time steps of 0.0001s
 		particle.setZ(6);
 		particle.setT(0);
 		particle.setV(0);
 		particle.drop(0.0001);
-		
-		System.out.println("As we decrease the increment in time, we get a more accurate result for the time taken to fall to the ground.");
+		System.out.println("Time taken: " + particle.getT() + "\t Velocity: " + particle.getV());
+
+		System.out.println(
+				"As we decrease the increment in time, we get a more accurate result for the time taken to fall to the ground and the velocity at the time of impact. This is due to z being closer to zero and not overshooting it as much");
 	}
 
 }
