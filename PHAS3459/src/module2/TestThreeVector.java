a99e30d002b33ce47f757346f3be82a95918a87b
Liam <kingliam28@ntlworld.com>
2015-10-17 14:41:25 +0100

    Comment on what each bit of code does and whether static or non-static.

--- PHAS3459/src/module2/TestThreeVector.java
+++ PHAS3459/src/module2/TestThreeVector.java
@@ -2,38 +2,44 @@ package module2;
 
 public class TestThreeVector {
 	public static void main(String[] args) {
+		// Creating three instances of the ThreeVector class
 		ThreeVector v1 = new ThreeVector(4, 3, 3);
 		ThreeVector v2 = new ThreeVector(3, 5, 2);
 		ThreeVector v3 = new ThreeVector(0, 0, 0);
 
-		System.out.println(v1 + "\n" + v1.unitVector() + "\n");
-		System.out.println(v2 + "\n" + v2.unitVector() + "\n");
-		System.out.println(v3 + "\n" + v3.unitVector());
+		// Printing the ThreeVectors and their corresponding unit vectors
+		System.out.println("Vector \n" + v1 + "\n" + "Unit \n" + v1.unitVector() + "\n");
+		System.out.println("Vector \n" + v2 + "\n" + "Unit \n" + v2.unitVector() + "\n");
+		System.out.println("Vector \n" + v3 + "\n" + "Unit \n" + v3.unitVector() + "\n");
 
-		// Scalar Product
-		System.out.println(ThreeVector.scalarProduct(v1, v2));
-		System.out.println(ThreeVector.scalarProduct(v1, v3));
+		// Scalar Product between v1, v2 and v1, v3 using static method.
+		System.out.println("Scalar Product of v1, v2 (static): " + ThreeVector.scalarProduct(v1, v2));
+		System.out.println("Scalar Product of v1, v2 (static): " + ThreeVector.scalarProduct(v1, v3));
 
-		System.out.println(v1.scalarProduct(v2));
-		System.out.println(v1.scalarProduct(v3));
+		// Scalar Product between v1, v2 and v1, v3 using non-static method.
+		System.out.println("Scalar Product of v1, v2 (non-static): " + v1.scalarProduct(v2));
+		System.out.println("Scalar Product of v1, v3 (non-static): " + v1.scalarProduct(v3));
 
-		// Vector Product
-		System.out.println(ThreeVector.vectorProduct(v1, v2));
-		System.out.println(ThreeVector.vectorProduct(v1, v3));
+		// Vector Product between v1, v2 and v1, v3 using static method.
+		System.out.println("Vector Product of v1, v2 (static): " + ThreeVector.vectorProduct(v1, v2));
+		System.out.println("Vector Product of v1, v3 (static): " + ThreeVector.vectorProduct(v1, v3));
 
-		System.out.println(v1.vectorProduct(v2));
-		System.out.println(v1.vectorProduct(v3));
+		// Vector Product between v1, v2 and v1, v2 using non-static method.
+		System.out.println("Vector Product of v1, v2 (non-static): " + v1.vectorProduct(v2));
+		System.out.println("Vector Product of v1, v3 (non-static): " + v1.vectorProduct(v3));
 
-		// Angle
-		System.out.println(ThreeVector.angle(v1, v2));
-		System.out.println(ThreeVector.angle(v1, v3));
+		// Angle between v1, v2 and v1, v3 using static method (radians).
+		// Radians.
+		System.out.println("Angle between v1, v2 (static), in radians: " + ThreeVector.angle(v1, v2));
+		System.out.println("Angle between v1, v3 (static), in radians: " + ThreeVector.angle(v1, v3));
 
-		System.out.println(v1.angle(v2));
-		System.out.println(v1.angle(v3));
+		// Angle between v1, v2 and v1, v3 using non-static method (radians).
+		System.out.println("Angle between v1, v2 (non-static) in radians: " + v1.angle(v2));
+		System.out.println("Angle between v1, v3 (non-static) in radians: " + v1.angle(v3));
 
 		// Printing out without toString() method.
 		System.out.println(v1);
-		System.out.println("Something to do with the memory, ***LOOK THIS UP LATER***");
+		System.out.println("Instead of displaying a nice formatted string, it displays the objects ID.");
 
 	}
 
