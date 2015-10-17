5dd13eabcb3da28033760826bf17bb49d12b929a
Liam <kingliam28@ntlworld.com>
2015-10-17 15:28:33 +0100

    Corrected modifiers and commented what methods and code does

--- PHAS3459/src/module2/Complex.java
+++ PHAS3459/src/module2/Complex.java
@@ -1,53 +1,68 @@
 package module2;
 
 public class Complex {
-	double real;
-	double imag;
+	private double real;
+	private double imag;
 
-	static Complex ONE = new Complex(1, 0);
-	static Complex ZERO = new Complex(0, 0);
-	static Complex I = new Complex(0, 1);
+	// Complex class versions of One, zero and I.
+	public final static Complex ONE = new Complex(1, 0);
+	public final static Complex ZERO = new Complex(0, 0);
+	public final static Complex I = new Complex(0, 1);
 
-	public Complex(double real, double imag) {
+	// Constructor to set/store real and imaginary parts of complex number.
+	public Complex(final double real, final double imag) {
 		this.real = real;
 		this.imag = imag;
 	}
 
+	// Returns the real component.
 	public double getReal() {
 		return real;
 	}
 
+	// Returns the imaginary component.
 	public double getImag() {
 		return imag;
 	}
 
-	double modulus() {
+	// Modulus of complex number. Square root of sum of real sq and imag sq.
+	public double modulus() {
 		return Math.sqrt(this.real * this.real + this.imag * this.imag);
 	}
 
-	double angle() {
+	// Returns angle in the complex plane anti-clockwise from pos real-axis
+	// measured in radians.
+	public double angle() {
 
 		if (this.imag >= 0) {
+			// Positive-Positive Quadrant
 			if (this.real >= 0) {
 				return Math.atan(this.imag / this.real);
 			}
+			// Negative-Positive Quadrant
 			return Math.PI - Math.atan(this.imag / -this.real);
 		}
 		if (this.real >= 0) {
+			// Positive-Negative Quadrant
 			return 2 * Math.PI - Math.atan(-this.imag / this.real);
 		}
+		// Negative-Negative Quadrant
 		return Math.PI + Math.atan(-this.imag / -this.real);
+
 	}
 
-	Complex conjugate() {
+	// Creates new Complex object with sign change on imaginary
+	public Complex conjugate() {
 		return new Complex(this.real, -this.imag);
 	}
 
-	Complex normalised() {
+	// Creates new Complex object with modulus 1, same angle
+	public Complex normalised() {
 		return new Complex(this.real / this.modulus(), this.imag / this.modulus());
 	}
 
-	boolean equals(Complex c) {
+	// Tests components of two Complex classes and determines if they're equal
+	public boolean equals(final Complex c) {
 		if (this.real == c.real && this.imag == c.imag) {
 			return true;
 		} else {
@@ -55,45 +70,55 @@ public class Complex {
 		}
 	}
 
+	// Allows object to be printed to nice formatted String
 	public String toString() {
+		// Deals with "+" or "-" sign depending on imaginary component
 		if (this.imag >= 0) {
 			return this.real + " + " + this.imag + "i";
 		}
 		return this.real + " - " + -this.imag + "i";
 	}
 
-	void setFromModulusAngle(double mag, double ang) {
+	// Takes a magnitude and angle and creates complex number x + iy
+	public void setFromModulusAngle(final double mag, final double ang) {
 
 		if (ang >= 0 && ang <= Math.PI / 2) {
-
+			// Positive-Positive Quadrant
 			this.real = mag * Math.cos(ang);
 			this.imag = mag * Math.sin(ang);
-
 		} else if (ang > Math.PI / 2 && ang <= Math.PI) {
+			// Negative-Positive Quadrant
 			this.real = -mag * Math.sin(ang - Math.PI / 2);
 			this.imag = mag * Math.cos(ang - Math.PI / 2);
 		} else if (ang > Math.PI && ang <= Math.PI * 3 / 2) {
+			// Negative-Negative Quadrant
 			this.real = -mag * Math.cos(ang - Math.PI);
 			this.imag = -mag * Math.sin(ang - Math.PI);
 		} else if (ang > Math.PI * 3 / 2 && ang <= Math.PI * 2) {
+			// Positive-Negative Quadrant
 			this.real = mag * Math.sin(ang - Math.PI * 3 / 2);
 			this.real = -mag * Math.cos(ang - Math.PI * 3 / 2);
 		}
 	}
 
-	static Complex add(Complex c1, Complex c2) {
+	// Complex Addition
+	public static Complex add(final Complex c1, final Complex c2) {
 		return new Complex(c1.real + c2.real, c1.imag + c2.imag);
 	}
 
-	static Complex subtract(Complex c1, Complex c2) {
+	// Complex subtraction
+	public static Complex subtract(final Complex c1, final Complex c2) {
 		return new Complex(c1.real - c2.real, c1.imag - c2.imag);
 	}
 
-	static Complex multiply(Complex c1, Complex c2) {
+	// Complex multiplication
+	public static Complex multiply(final Complex c1, final Complex c2) {
 		return new Complex(c1.real * c2.real - c1.imag * c2.imag, c1.real * c2.imag + c2.real * c1.imag);
 	}
 
-	static Complex divide(Complex c1, Complex c2) {
+	// Complex division
+	public static Complex divide(final Complex c1, final Complex c2) {
+		// Normalises so denominator is a real value
 		return new Complex(Complex.multiply(c1, c2.conjugate()).real / (c2.real * c2.real + c2.imag * c2.imag),
 				Complex.multiply(c1, c2.conjugate()).imag / (c2.real * c2.real + c2.imag * c2.imag));
 	}
--- PHAS3459/src/module2/TestComplex.java
+++ PHAS3459/src/module2/TestComplex.java
@@ -2,14 +2,21 @@ package module2;
 
 public class TestComplex {
 	public static void main(String[] args) {
+		// Creating two new Complex objects
 		Complex c1 = new Complex(1, 2);
-		Complex c2 = new Complex(-2, 1);
-		
+		Complex c2 = new Complex(-2, -1);
+
+		// c1 multiplied by c2
 		System.out.println(Complex.multiply(c1, c2));
+		// c1 divided by c2
 		System.out.println(Complex.divide(c1, c2));
+		// c1 multiplied by I
 		System.out.println(Complex.multiply(c1, Complex.I));
+		// c1 divided by Zero
 		System.out.println(Complex.divide(c1, Complex.ZERO));
+		// c1 multiplied with the conjugate of c1
 		System.out.println(Complex.multiply(c1, c1.conjugate()));
+		// c2 multiplied with the conjugate of c2
 		System.out.println(Complex.multiply(c2, c2.conjugate()));
 
 	}
