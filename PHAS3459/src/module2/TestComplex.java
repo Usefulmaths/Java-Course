818cd2b3686935e4226c773921ea68d53e46012f
Liam <kingliam28@ntlworld.com>
2015-10-18 12:22:04 +0100

    Added some clarification in console what each line is

--- PHAS3459/src/module2/TestComplex.java
+++ PHAS3459/src/module2/TestComplex.java
@@ -7,17 +7,17 @@ public class TestComplex {
 		Complex c2 = new Complex(-2, -1);
 
 		// c1 multiplied by c2
-		System.out.println(Complex.multiply(c1, c2));
+		System.out.println("c1 * c2: " + Complex.multiply(c1, c2));
 		// c1 divided by c2
-		System.out.println(Complex.divide(c1, c2));
+		System.out.println("c1 / c2: " + Complex.divide(c1, c2));
 		// c1 multiplied by I
-		System.out.println(Complex.multiply(c1, Complex.I));
+		System.out.println("c1 * I: " + Complex.multiply(c1, Complex.I));
 		// c1 divided by Zero
-		System.out.println(Complex.divide(c1, Complex.ZERO));
+		System.out.println("c1 / 0: " + Complex.divide(c1, Complex.ZERO));
 		// c1 multiplied with the conjugate of c1
-		System.out.println(Complex.multiply(c1, c1.conjugate()));
+		System.out.println("c1 * conj(c1): " + Complex.multiply(c1, c1.conjugate()));
 		// c2 multiplied with the conjugate of c2
-		System.out.println(Complex.multiply(c2, c2.conjugate()));
+		System.out.println("c2 * conj(c2): " + Complex.multiply(c2, c2.conjugate()));
 
 	}
 }
