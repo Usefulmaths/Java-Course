d2b21b9bf41b72a145a2284e6fd3d4c72e41183e
Liam <kingliam28@ntlworld.com>
2015-10-18 12:25:01 +0100

    Added clarification of time increment for particles in console

--- PHAS3459/src/module2/ParticleMain.java
+++ PHAS3459/src/module2/ParticleMain.java
@@ -7,35 +7,40 @@ public class ParticleMain {
 		// Particle dropping from height 6m in time steps of 0.5s
 		particle.setZ(6);
 		particle.drop(0.5);
-		System.out.println("Time taken: " + particle.getT() + "\t Velocity: " + particle.getV());
+		System.out.println(
+				"Time Step: " + 0.5 + "\t\t " + "Time taken: " + particle.getT() + "\t\t Velocity: " + particle.getV());
 
 		// Particle dropping from height 6m in time steps of 0.1s
 		particle.setZ(6);
 		particle.setT(0);
 		particle.setV(0);
 		particle.drop(0.1);
-		System.out.println("Time taken: " + particle.getT() + "\t Velocity: " + particle.getV());
+		System.out.println(
+				"Time Step: " + 0.1 + "\t\t " + "Time taken: " + particle.getT() + "\t Velocity: " + particle.getV());
 
 		// Particle dropping from height 6m in time steps of 0.01s
 		particle.setZ(6);
 		particle.setT(0);
 		particle.setV(0);
 		particle.drop(0.01);
-		System.out.println("Time taken: " + particle.getT() + "\t Velocity: " + particle.getV());
+		System.out.println(
+				"Time Step: " + 0.01 + "\t\t " + "Time taken: " + particle.getT() + "\t Velocity: " + particle.getV());
 
 		// Particle dropping from height 6m in time steps of 0.001s
 		particle.setZ(6);
 		particle.setT(0);
 		particle.setV(0);
 		particle.drop(0.001);
-		System.out.println("Time taken: " + particle.getT() + "\t Velocity: " + particle.getV());
+		System.out.println(
+				"Time Step: " + 0.001 + "\t " + "Time taken: " + particle.getT() + "\t Velocity: " + particle.getV());
 
 		// Particle dropping from height 6m in time steps of 0.0001s
 		particle.setZ(6);
 		particle.setT(0);
 		particle.setV(0);
 		particle.drop(0.0001);
-		System.out.println("Time taken: " + particle.getT() + "\t Velocity: " + particle.getV());
+		System.out.println(
+				"Time Step: " + 0.0001 + "\t " + "Time taken: " + particle.getT() + "\t Velocity: " + particle.getV());
 
 		System.out.println(
 				"As we decrease the increment in time, we get a more accurate result for the time taken to fall to the ground and the velocity at the time of impact. This is due to z being closer to zero and not overshooting it as much");
