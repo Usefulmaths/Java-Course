4b8ab7de4765fe1e7611df01943783f1023d272f
Liam <kingliam28@ntlworld.com>
2015-10-18 12:37:34 +0100

    Made the variable "g" static

--- PHAS3459/src/module2/FallingParticle.java
+++ PHAS3459/src/module2/FallingParticle.java
@@ -7,7 +7,7 @@ public class FallingParticle {
 	private double t = 0;
 	public double z;
 	private double v = 0;
-	private final double g = 9.81;
+	private static final double g = 9.81;
 
 	// Constructor to set/store mass (kg) and drag coefficient (no units) of
 	// particle
