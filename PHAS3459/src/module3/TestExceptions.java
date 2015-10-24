02b1eed1c5a42540aec89b1516d42aa529c7e4ad
Liam <kingliam28@ntlworld.com>
2015-10-24 21:49:07 +0100

    Updated comments.

--- PHAS3459/src/module3/TestExceptions.java
+++ PHAS3459/src/module3/TestExceptions.java
@@ -6,12 +6,14 @@ public class TestExceptions {
 		Complex c1 = new Complex(1, 7);
 		Complex c2 = new Complex(0, 0);
 
+		// Normalisation Exception
 		try {
 			c2.normalised();
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
 
+		// Division Exception
 		try {
 			Complex.divide(c1, c2);
 		} catch (Exception e) {
@@ -22,12 +24,14 @@ public class TestExceptions {
 		ThreeVector v1 = new ThreeVector(1, 1, 1);
 		ThreeVector v2 = new ThreeVector(0, 0, 0);
 
+		// Unit Vector Exception
 		try {
 			v2.unitVector();
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
 
+		// Angle Exception
 		try {
 			v1.angle(v2);
 		} catch (Exception e) {
@@ -35,43 +39,40 @@ public class TestExceptions {
 		}
 
 		// Testing FallingParticle Exceptions
-		
-		//Mass Exception
+
+		// Mass Exception
 		try {
 			FallingParticle particle1 = new FallingParticle(-1, 2);
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
 
-		//Drag Coefficient Exception
+		// Drag Coefficient Exception
 		try {
 			FallingParticle particle2 = new FallingParticle(4.2, -4.6);
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
-		
-		//Height Exception
+
+		// Height Exception
 		FallingParticle particle3 = new FallingParticle(4.2, 3.2);
-		try{
+		try {
 			particle3.setZ(-0.3);
-		}
-		catch(Exception e){
+		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
-		
-		//deltaT Exception
-		
-		try{
+
+		// Time Increment Exception
+
+		try {
 			particle3.doTimeStep(-0.1);
-		}
-		catch(Exception e){
+		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
-		
-		try{
+
+		try {
 			particle3.drop(-0.1);
-		}
-		catch(Exception e){
+		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
 
