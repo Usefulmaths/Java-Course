83378229af8478aa14a60e3dc247f8a1df0034e7
Liam <kingliam28@ntlworld.com>
2015-10-29 12:21:05 +0000

    Added Try/Catches, Reads from URL, Writes into a file, Analysis numbers

--- PHAS3459/src/module4/NumericalReader.java
+++ PHAS3459/src/module4/NumericalReader.java
@@ -8,25 +8,97 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.lang.reflect.Array;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class NumericalReader {
-	private int minValue;
-	private int maxValue;
-	private int nValues;
-	private int sumOfValues;
-	File file;
-	FileWriter fw;
+	private static double minValue;
+	private static double maxValue;
+	private static int nValues;
+	private static double sumOfValues;
+	private static ArrayList<String> keepTrackOfNumbers = new ArrayList();
+	private File file;
+	private FileWriter fw;
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
+		numbers1();
+		numbers2();
+
+	}
+
+	private static void numbers1() {
 		NumericalReader nr = new NumericalReader();
+		BufferedReader brNumbers1 = null;
 
-		String dir = getOutputDirectory();
-		System.out.println(dir);
-		nr.analysisStart(dir);
-		nr.analyseData("1 2 3 4 5");
+		try {
+			brNumbers1 = brFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/data/module4/module4_data1.txt");
+		} catch (IOException e) {
+			System.out.println("Error reading in from BufferedReader: " + e);
+		}
 
+		String dir;
+		dir = getOutputDirectory();
+
+		try {
+			nr.analysisStart(dir + File.separator + "numbers1.txt");
+		} catch (IOException e) {
+			System.out.println("Error creating number file: " + e);
+		}
+
+		String line;
+		try {
+			while ((line = brNumbers1.readLine()) != null) {
+				try {
+					nr.analyseData(line);
+				} catch (IOException e) {
+					System.out.println("Error writing to number file: " + e);
+				}
+			}
+		} catch (IOException e) {
+			System.out.println("Error reading line via BufferedReader: " + e);
+		}
+		updateVariblesFromArray(keepTrackOfNumbers);
+
+		nr.analysisEnd();
+	}
+
+	private static void numbers2() {
+		NumericalReader nr = new NumericalReader();
+		BufferedReader brNumbers1 = null;
+
+		try {
+			brNumbers1 = brFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/data/module4/module4_data2.txt");
+		} catch (IOException e) {
+			System.out.println("Error reading in from BufferedReader: " + e);
+		}
+
+		String dir;
+		dir = getOutputDirectory();
+
+		try {
+			nr.analysisStart(dir + File.separator + "numbers2.txt");
+		} catch (IOException e) {
+			System.out.println("Error creating number file: " + e);
+		}
+
+		String line;
+		try {
+			while ((line = brNumbers1.readLine()) != null) {
+				try {
+					nr.analyseData(line);
+				} catch (IOException e) {
+					System.out.println("Error writing to number file: " + e);
+				}
+			}
+		} catch (IOException e) {
+			System.out.println("Error reading line via BufferedReader: " + e);
+		}
+		updateVariblesFromArray(keepTrackOfNumbers);
+
+		nr.analysisEnd();
 	}
 
 	private static String getOutputDirectory() {
@@ -34,8 +106,12 @@ public class NumericalReader {
 		System.out.print("Enter directory location: ");
 		Scanner scanner = new Scanner(System.in);
 		String location = scanner.nextLine();
-
-		return location;
+		if (location != "this") {
+			return location;
+		} else {
+			location = System.getProperty("user.home");
+			return location;
+		}
 
 	}
 
@@ -45,11 +121,11 @@ public class NumericalReader {
 		InputStreamReader isr = new InputStreamReader(in);
 		BufferedReader br = new BufferedReader(isr);
 
-		return new BufferedReader(isr);
+		return br;
 
 	}
 
-	void analysisStart(String dataFile) throws IOException {
+	private void analysisStart(String dataFile) throws IOException {
 		file = new File(dataFile);
 		fw = new FileWriter(file);
 
@@ -58,23 +134,63 @@ public class NumericalReader {
 		nValues = 0;
 		sumOfValues = 0;
 
+		keepTrackOfNumbers.removeAll(keepTrackOfNumbers);
 	}
 
-	void analyseData(String line) throws IOException {
-		if (!line.isEmpty() || Character.isDigit(line.charAt(0))) {
+	private void analyseData(String line) throws IOException {
+		if (!line.matches("([a-zA-Z].*)|\\W")) {
 
 			Scanner scanner = new Scanner(line);
 			BufferedWriter bw = new BufferedWriter(fw);
 
 			while (scanner.hasNext()) {
 				String str = scanner.next();
+
+				keepTrackOfNumbers.add(str);
+
 				System.out.println(str);
 
 				bw.write(str + "\n");
 
 			}
-			bw.close();
+			bw.flush();
 		}
+
+	}
+
+	private static void updateVariblesFromArray(ArrayList list) {
+		ArrayList<String> listString = list;
+		ArrayList<Double> listDouble = new ArrayList();
+
+		for (String i : listString) {
+			double stringToDouble = Double.parseDouble(i);
+			listDouble.add(stringToDouble);
+		}
+
+		minValue = Collections.min(listDouble);
+		maxValue = Collections.max(listDouble);
+		nValues = listDouble.size();
+		sumOfValues = sumOfArray(listDouble);
+
+	}
+
+	private static double sumOfArray(ArrayList list) {
+		ArrayList<Double> listDouble = list;
+		double sum = 0;
+		for (double i : listDouble) {
+			sum += i;
+		}
+
+		return sum;
+	}
+
+	private void analysisEnd() {
+		System.out.println("Minimum value: " + minValue);
+		System.out.println("Maximum value: " + maxValue);
+		System.out.println("Average value: " + sumOfValues / nValues);
+		System.out.println("Number of values read: " + nValues);
+
+		System.out.println(System.getProperty("user.home"));
 	}
 
 }
