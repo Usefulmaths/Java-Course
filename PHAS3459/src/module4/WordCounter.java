c5f05c13f6478252495fd387cbc56454531de9ff
Liam <kingliam28@ntlworld.com>
2015-11-01 00:03:40 +0000

    Added in additional exceptions and comments.

--- PHAS3459/src/module4/WordCounter.java
+++ PHAS3459/src/module4/WordCounter.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Scanner;
 
@@ -16,12 +17,17 @@ public class WordCounter {
 			int numberOfWords = countWordsInResource(br);
 
 			System.out.println("Number of words in the URL: " + numberOfWords);
+		} catch (MalformedURLException e) {
+			System.out.println("Error while parsing in URL: " + e.getMessage());
+
 		} catch (IOException e) {
-			System.out.println(e);
+			System.out.println("Error reading in resource: " + e.getMessage());
 		}
 	}
 
-	private static BufferedReader brFromURL(String urlName) throws IOException {
+	// Creates a BufferedReader Object from parsing a URL string
+	private static BufferedReader brFromURL(String urlName) throws IOException, MalformedURLException {
+
 		URL u = new URL(urlName);
 		InputStream in = u.openStream();
 		InputStreamReader isr = new InputStreamReader(in);
@@ -31,7 +37,10 @@ public class WordCounter {
 
 	}
 
+	// Counts the number of words within the given URL data stored within a
+	// BufferedReader
 	private static int countWordsInResource(BufferedReader dataAsBR) throws IOException {
+
 		BufferedReader br = dataAsBR;
 		Scanner scanner = new Scanner(br);
 		String line;
