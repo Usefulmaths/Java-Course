1cb29374b4b1d95733b0bd9a81cef546cc077a1c
Liam <kingliam28@ntlworld.com>
2015-11-02 00:10:30 +0000

    Functional Programming Java 8 for counting words.

--- PHAS3459/src/module4/WordCounter.java
+++ PHAS3459/src/module4/WordCounter.java
@@ -6,50 +6,47 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WordCounter {
 
 	public static void main(String[] args) throws IOException {
 
 		try {
-			BufferedReader br = brFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/data/module4/module4_text.txt");
-			int numberOfWords = countWordsInResource(br);
+			final BufferedReader br = brFromURL(
+					"http://www.hep.ucl.ac.uk/undergrad/3459/data/module4/module4_text.txt");
+			final int numberOfWords = countWordsInResource(br);
 
 			System.out.println("Number of words in the URL: " + numberOfWords);
-		} catch (MalformedURLException e) {
+		} catch (final MalformedURLException e) {
 			System.out.println("Error while parsing in URL: " + e.getMessage());
 
-		} catch (IOException e) {
+		} catch (final IOException e) {
 			System.out.println("Error reading in resource: " + e.getMessage());
 		}
 	}
 
 	// Creates a BufferedReader Object from parsing a URL string
-	private static BufferedReader brFromURL(String urlName) throws IOException, MalformedURLException {
-
-		URL u = new URL(urlName);
-		InputStream in = u.openStream();
-		InputStreamReader isr = new InputStreamReader(in);
-		BufferedReader br = new BufferedReader(isr);
-
-		return new BufferedReader(isr);
-
+	private static BufferedReader brFromURL(final String urlName) throws IOException, MalformedURLException {
+		final URL url = new URL(urlName);
+		final InputStream stream = url.openStream();
+		return new BufferedReader(new InputStreamReader(stream));
 	}
 
-	// Counts the number of words within the given URL data stored within a
+	// Counts the number of words within the given UjRL data stored within a
 	// BufferedReader
-	private static int countWordsInResource(BufferedReader dataAsBR) throws IOException {
+	private static int countWordsInResource(final BufferedReader dataAsBR) throws IOException {		
+		return dataAsBR.lines()
+				.mapToInt(WordCounter::countWordsInLine)
+				.sum();
+	}
 
-		BufferedReader br = dataAsBR;
-		Scanner scanner = new Scanner(br);
-		String line;
-		int counter = 0;
-
-		while (scanner.hasNext()) {
-			scanner.next();
-			counter++;
+	private static int countWordsInLine(final String line) {
+		if (line.isEmpty()) {
+			return 0;
+		} else {
+			return line.split(" ").length;
 		}
-		return counter;
 	}
 }
