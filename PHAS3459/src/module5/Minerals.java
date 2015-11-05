96e4ce2fa853c5024f6d96571f5d152c8ed5bc78
Liam <kingliam28@ntlworld.com>
2015-11-05 00:33:05 +0000

    Formatting code nicely using methods.

--- PHAS3459/src/module5/Minerals.java
+++ PHAS3459/src/module5/Minerals.java
@@ -1,86 +1,107 @@
 package module5;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Map.Entry;
 import java.util.Scanner;
 
 public class Minerals {
+
+	private final HashMap<Integer, Double> hashMapMass = new HashMap<Integer, Double>();
+	private final HashMap<Integer, String> hashMapLocation = new HashMap<Integer, String>();
+
 	public static void main(String[] args) {
 
-		HashMap<Integer, Double> hashMap1 = new HashMap<Integer, Double>();
-		HashMap<Integer, String> hashMap2 = new HashMap<Integer, String>();
+		final Minerals minerals = new Minerals();
 
+		minerals.importMass("http://www.hep.ucl.ac.uk/undergrad/3459/data/module5/module5-samples.txt");
+		minerals.importLocation("http://www.hep.ucl.ac.uk/undergrad/3459/data/module5/module5-locations.txt");
+
+		final int minimumMassCode = minerals.minimumMassCode();
+		final int maximumMassCode = minerals.maximumMassCode();
+
+		minerals.informationAboutCode(minimumMassCode);
+		minerals.informationAboutCode(maximumMassCode);
+
+	}
+
+	private static Scanner urlToBr(final String urlString) throws IOException {
+		final URL url = new URL(urlString);
+		final InputStream stream = url.openStream();
+
+		return new Scanner(stream);
+	}
+
+	private void importMass(final String url) {
+		Scanner scannerMass = null;
 		try {
-			BufferedReader br1 = urlToBr("http://www.hep.ucl.ac.uk/undergrad/3459/data/module5/module5-samples.txt");
-			BufferedReader br2 = urlToBr("http://www.hep.ucl.ac.uk/undergrad/3459/data/module5/module5-locations.txt");
-
-			Scanner scanner1 = new Scanner(br1);
-			Scanner scanner2 = new Scanner(br2);
-
-			while (scanner1.hasNext()) {
-				String line = scanner1.nextLine();
-				String[] pairArray = line.split("  ");
-
-				hashMap1.put(Integer.parseInt(pairArray[0]), Double.parseDouble(pairArray[1]));
-
-			}
-
-			while (scanner2.hasNext()) {
-				String line = scanner2.nextLine();
-				String[] pairArray = line.split("  ");
-
-				hashMap2.put(Integer.parseInt(pairArray[1]), pairArray[0]);
-			}
-
-			double minValue = Double.POSITIVE_INFINITY;
-			double maxValue = Double.NEGATIVE_INFINITY;
-			for (double key : hashMap1.values()) {
-				if (key < minValue) {
-					minValue = key;
-				}
-				if (key > maxValue) {
-					maxValue = key;
-				}
-			}
-
-			int minValueKey = 0;
-			int maxValueKey = 0;
-			for (Entry<Integer, Double> e : hashMap1.entrySet()) {
-				if (e.getValue() == minValue) {
-					minValueKey = e.getKey();
-				} else if (e.getValue() == maxValue) {
-					maxValueKey = e.getKey();
-				}
-			}
-
-			if (hashMap1.containsKey(minValueKey) && hashMap2.containsKey(minValueKey)) {
-				System.out.println("Code Number: " + minValueKey + "\nMass: " + minValue + "\nLocation: "
-						+ hashMap2.get(minValueKey) + "\n");
-			}
-
-			if (hashMap1.containsKey(maxValueKey) && hashMap2.containsKey(maxValueKey)) {
-				System.out.println("Code Number: " + maxValueKey + "\nMass: " + maxValue + "\nLocation: "
-						+ hashMap2.get(maxValueKey));
-			}
-
+			scannerMass = urlToBr(url);
 		} catch (IOException e) {
 			System.out.println(e.getMessage());
 		}
 
+		while (scannerMass.hasNext()) {
+			final int code = scannerMass.nextInt();
+			final double mass = scannerMass.nextDouble();
+
+			hashMapMass.put(code, mass);
+
+		}
+
 	}
 
-	private static BufferedReader urlToBr(String urlString) throws IOException {
-		URL url = new URL(urlString);
-		InputStream stream = url.openStream();
+	private void importLocation(final String url) {
+		Scanner scannerLocation = null;
+		try {
+			scannerLocation = urlToBr(url);
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
 
-		return new BufferedReader(new InputStreamReader(stream));
+		while (scannerLocation.hasNext()) {
+			final String location = scannerLocation.next("[A-Za-z]*");
+			final int code = scannerLocation.nextInt();
+
+			hashMapLocation.put(code, location);
+		}
+	}
+
+	private int minimumMassCode() {
+		int codeOfValue = 0;
+		double minValue = Double.POSITIVE_INFINITY;
+
+		for (HashMap.Entry<Integer, Double> mineral : hashMapMass.entrySet()) {
+
+			if (mineral.getValue() < minValue) {
+				minValue = mineral.getValue();
+				codeOfValue = mineral.getKey();
+			}
+
+		}
+		return codeOfValue;
+
+	}
+
+	private int maximumMassCode() {
+		int codeOfValue = 0;
+		double maxValue = Double.NEGATIVE_INFINITY;
+
+		for (HashMap.Entry<Integer, Double> mineral : hashMapMass.entrySet()) {
+
+			if (mineral.getValue() > maxValue) {
+				maxValue = mineral.getValue();
+				codeOfValue = mineral.getKey();
+			}
+
+		}
+		return codeOfValue;
+
+	}
+
+	private void informationAboutCode(final int code) {
+		System.out.println("Code Number: " + code + "\n" + "Mass: " + this.hashMapMass.get(code) + "\n" + "Location: "
+				+ this.hashMapLocation.get(code) + "\n");
 	}
 
 }
