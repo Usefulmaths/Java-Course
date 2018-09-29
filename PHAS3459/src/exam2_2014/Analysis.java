d2332f35816c43c116011c10d3404031b9537a35
Liam <kingliam28@ntlworld.com>
2018-09-29 15:22:01 +0100

    Remove unnecessary directories

--- PHAS3459/src/exam2_2014/Analysis.java
+++
@@ -1,5 +0,0 @@
-package exam2_2014;
-
-public class Analysis {
-
-}
--- PHAS3459/src/exam2_2014/Statistic.java
+++
@@ -1,7 +0,0 @@
-package exam2_2014;
-
-import java.util.List;
-
-public interface Statistic {
-	double calculation(List<Tide> tides);
-}
